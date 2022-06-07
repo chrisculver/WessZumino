8440b4104c808cc9879e3e7e772ab5f267c2c14e
chris <chris.cb.culver@gmail.com>
2022-06-07 11:22:14 +0100

    Pulling in some PS utilities

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,5 @@
+# Read me 
+
+This is a directory for the studies of the Wess Zumino model
+
+Starting with 1+1 D N=1.
---
+++ .ipynb_checkpoints/binary_encodings-checkpoint.py
@@ -0,0 +1,10 @@
+# fyi the module name encodings is taken
+
+def standard_encode(i,N):
+    format_style = '0' + str(N) + 'b'
+    return format(i, format_style)
+
+
+def gray_code(i,N):
+    gray=i^(i>>1)
+    return "{0:0{1}b}".format(gray,N)
