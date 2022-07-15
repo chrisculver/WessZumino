e71e08e79b4515884418ef6165cb52cefa4cc8b9
chrisculver <chris.cb.culver@gmail.com>
2022-07-15 02:00:19 +0100

    Know why it doesn't work but need to fix

--- ham_to_matrix.py
+++ ham_to_matrix.py
@@ -66,7 +66,7 @@ def convert_to_matrix(expr, cutoff, Nsites, aops, adags):
     # now need to drop all the buffers...
     #for i
         
-    return fullHam.astype(complex)
+    return fullHam
 
 
 
@@ -141,5 +141,83 @@ def convert_term_to_matrix(term, cutoff, Nsites, aops, adags):
             #fullMat=np.kron(fullMat,np.eye(2))
             fullMat=np.kron(fullMat,np.eye(2))
 
+            
+    return coef*fullMat    
+
+
+
+
+
+def convert_boson_to_matrix(expr, cutoff, Nsites, aops, adags):
+    # start with a matrix of zeros
+    fullHam = np.zeros([(cutoff**Nsites),(cutoff**Nsites)]).astype(np.complex64)
+
+    # convert each term to matrix and sum up
+    for t in expr.args:
+        fullHam=fullHam+convert_boson_term_to_matrix(t,cutoff,Nsites, aops, adags)
+        
+    # now need to drop all the buffers...
+    #for i
+        
+    return fullHam
+
+
+
+
+
+# This is WRONG!
+# need to multiply from left to right no just kron everything.
+# this doesn't handle ad0 a1 a0 a1d ad0^3 a1 a1d
+def convert_boson_term_to_matrix(term, cutoff, Nsites, aops, adags):
+    coef=1
+    start=0
+    
+    if getattr(term.args[0],'__module__', None)=='sympy.core.numbers':
+        coef=term.args[0]
+        start=1
+        
+    mats={}
+    for t in term.args[start:]:
+        boson=False
+        isPow=False
+        if type(t)==sp.core.power.Pow:
+            isPow=True
+            if hasattr(t.args[0],'name'):
+                if t.args[0].name[0]=='a':
+                    boson=True
+        if hasattr(t,'name'):
+            if t.name[0]=='a':
+                boson=True
+        
+        siteSubs = site_subs(cutoff,Nsites, aops, adags)
+    
+        if boson:
+            if isPow:              
+                siteSubs = site_subs(cutoff+t.args[1],Nsites, aops, adags)
+                #print(siteSubs)
+                mats['b'+str(t.args[0].site)]=np.linalg.matrix_power(np.array(
+                        t.args[0].subs(siteSubs).doit()).astype(np.complex64),t.args[1])[:cutoff,:cutoff]
+            
+            else:
+                siteSubs = site_subs(cutoff,Nsites, aops, adags)
+                mats['b'+str(t.site)]=np.array(
+                    t.subs(siteSubs).doit()).astype(np.complex64)
+        else:
+            print("non-boson term??!?!?!")
+            
+    #fullMat=np.eye((cutoff**Nsites)*(2**Nsites)).astype(np.complex64)
+    fullMat=1
+    for i in range(Nsites):
+    #    if i==0:
+    #        offset=0
+    #    else:
+    #        offset=((cutoff**i)*(2**i))
+        if 'b'+str(i) in mats:
+            print(mats['b'+str(i)])
+    #        fullMat[offset:offset+cutoff,offset:offset+cutoff]=mats['b'+str(i)]
+            fullMat=np.kron(fullMat,mats['b'+str(i)])
+        else:
+        #    fullMat[offset:offset+cutoff,offset:offset+cutoff]=mats['b'+str(i)]
+            fullMat=np.kron(fullMat,np.eye(cutoff))
             
     return coef*fullMat    
